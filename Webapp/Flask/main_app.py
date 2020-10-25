f3a0906f2f15747ef386358de00595ef97045ba6
Mohammad Ahmad <md.ahmad0652@gmail.com>
2020-10-26 00:44:11 +0530

    Added all flask files

---
+++ Webapp/Flask/main_app.py
@@ -0,0 +1,20 @@
+from ml_model.predict import pred
+
+import os
+from flask import Flask, render_template, request, make_response, jsonify, send_file
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+# Set up the main route
+@app.route('/predict', methods=['POST'])
+def home():
+	print("Action Initiated")
+	review = request.json['review']
+	prediction = pred(review)
+	print(prediction)
+	return prediction
+
+if __name__ == '__main__':
+    app.run()
