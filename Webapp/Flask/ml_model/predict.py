39f4913d47ea3ecd6ccb269192389469a0bfddca
Mohammad Ahmad <md.ahmad0652@gmail.com>
2020-10-27 09:54:41 +0530

    Some code changes

--- Webapp/Flask/ml_model/predict.py
+++ Webapp/Flask/ml_model/predict.py
@@ -5,57 +5,45 @@ import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import torch.nn.functional as F
+from transformers import RobertaTokenizer, RobertaModel, AdamW, get_linear_schedule_with_warmup
 
 
 import os
 cwd = os.getcwd()
 
 import pickle
-b =  pickle.load(open(cwd + '/ml_model/vocab.pickle','rb'))
+b =  pickle.load(open(cwd + '\\ml_model\\vocab.pickle','rb'))
 
 vocab = b['vocab']
 
 
 
-class LSTM(nn.Module):
-
-    def __init__(self, dimension=128):
-        super(LSTM, self).__init__()
-
-        self.embedding = nn.Embedding(19493, 500)
-        self.dimension = dimension
-        self.lstm = nn.LSTM(input_size=500,
-                            hidden_size=dimension,
-                            num_layers=1,
-                            batch_first=True,
-                            bidirectional=True)
-        self.drop = nn.Dropout(p=0.3)
-
-        self.fc = nn.Linear(2*dimension, 1)
-
-    def forward(self, text, text_len):
-        text_emb = self.embedding(text)
-        packed_input = pack_padded_sequence(text_emb, text_len, batch_first=True, enforce_sorted=False)
-        packed_output, _ = self.lstm(packed_input)
-        output, _ = pad_packed_sequence(packed_output, batch_first=True)
-
-        out_forward = output[range(len(output)), text_len - 1, :self.dimension]
-        out_reverse = output[:, 0, self.dimension:]
-        out_reduced = torch.cat((out_forward, out_reverse), 1)
-        text_fea = self.drop(out_reduced)
-
-        text_fea = self.fc(text_fea)
-        text_fea = torch.squeeze(text_fea, 1)
-        text_out = torch.sigmoid(text_fea)
-
-        return text_out
+class ROBERTA(torch.nn.Module):
+    def __init__(self, dropout_rate=0.3):
+        super(ROBERTA, self).__init__()
+        self.roberta = RobertaModel.from_pretrained('roberta-base')
+        self.d1 = torch.nn.Dropout(dropout_rate)
+        self.l1 = torch.nn.Linear(768, 64)
+        self.bn1 = torch.nn.LayerNorm(64)
+        self.d2 = torch.nn.Dropout(dropout_rate)
+        self.l2 = torch.nn.Linear(64, 2)
+        
+    def forward(self, input_ids, attention_mask):
+        _, x = self.roberta(input_ids=input_ids, attention_mask=attention_mask)
+        x = self.d1(x)
+        x = self.l1(x)
+        x = self.bn1(x)
+        x = torch.nn.Tanh()(x)
+        x = self.d2(x)
+        x = self.l2(x)
+        return x
 
 
-model = LSTM()
+model = ROBERTA()
 
 #moving to gpu
 
-state_dict = torch.load(cwd + '/ml_model/modelFinal.pth', map_location=torch.device('cpu'))
+state_dict = torch.load(cwd + '\\ml_model\\final_model.pth', map_location=torch.device('cpu'))
 model.load_state_dict(state_dict, strict=False)
 
 
@@ -65,10 +53,11 @@ def pred(text):
                       if word in vocab.keys()])
     word_seq = np.expand_dims(word_seq,axis=0)
     t = torch.from_numpy(word_seq).to(torch.int64)
-    length = torch.LongTensor([1])
-    output = model(t, length)
+    mask = (t != 1).type(torch.uint8)
+
+    output = model(t, attention_mask=mask)
     print("Got output - ",output)
-    pro = (output.item())
-    status = "positive" if pro < 0.5 else "negative"
+    pro = torch.argmax(output, axis=-1).tolist()[0]
+    status = "positive" if pro == 1 else "negative"
     return status
 
