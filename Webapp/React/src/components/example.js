b3bbecb54a5d4cacec9a776c25ac010e8a860599
ahmadkhan242 <md.ahmad0652@gmail.com>
2020-12-01 03:40:26 +0530

    some UI improved

--- Webapp/React/src/components/example.js
+++ Webapp/React/src/components/example.js
@@ -1,24 +1,37 @@
 import React, { Component} from "react";
 
+import {
+  Badge,
+  ListGroup
+} from "react-bootstrap";
+
 class EXAMPLE extends Component {
+
   render() {
     return (
-      <div style={{padding:"50px", background:"#c4ffe6", height:"100%"}}>
-            <h3 style={{alignContent:"center"}}>This is Example section.</h3>
+      <div style={{padding:"50px", height:"100%"}}>
+            <h3 style={{alignContent:"center"}}>Sample review to test model.</h3>
+            <ListGroup>
+            <ListGroup.Item variant="info">
             <h4 style={{marginBottom:"0"}}>Example 1</h4>
-            <p style={{margin:"0"}}>I find it very intriguing that Lee Radziwill, Jackie Kennedy's sister and the cousin of these women, would encourage the Maysles' to make "Big Edie" and 
-              "Little Edie" the subject of a film. They certainly could be considered the "skeletons" in the family closet. The extra features on the DVD include several 
-              contemporary fashion designers crediting some of their ideas to these oddball women. I'd say that anyone interested in fashion would find the discussion by these 
-              designers fascinating. (i.e. "Are they nuts? Or am I missing something?"). 
-              This movie is hard to come by. Netflix does not have it. Facets does, though.</p>
-              <h4 style={{marginBottom:"0"}}>Example 2</h4>
+            <p style={{margin:"0"}}>No one expects the Star Trek movies to be high art, but the fans do expect a movie that is as good as some of the best episodes. Unfortunately, this movie had a muddled, 
+            implausible plot that just left me cringing - this is by far the worst of the nine (so far) movies. Even the chance to watch the well known characters interact in another movie can't save this 
+            movie - including the goofy scenes with Kirk, Spock and McCoy at Yosemite. I would say this movie is not worth a rental, 
+            and hardly worth watching, however for the True Fan who needs to see all the movies, renting this movie is about the only way you'll see it - even the cable channels avoid this movie.</p>
+            </ListGroup.Item>
+            <ListGroup.Item variant="info">
+            <h4 style={{marginBottom:"0"}}>Example 2</h4>
             <p style={{margin:"0"}}>Encouraged by the positive comments about this film on here I was looking forward to watching this film. Bad mistake. I've seen 950+ films and this is 
               truly one of the worst of them - it's awful in almost every way: editing, pacing, storyline, 'acting,' soundtrack (the film's only song - a lame country 
               tune - is played no less than four times). The film looks cheap and nasty and is boring in the extreme. Rarely have I been so happy to see the end credits
                of a film. The only thing that prevents me giving this a 1-score is Harvey Keitel - while this is far from his best performance he at least 
                seems to be making a bit of an effort. One for Keitel obsessives only.</p>
-               <h4 style={{marginBottom:"0"}}>Example 3</h4>
+               </ListGroup.Item>
+            <ListGroup.Item variant="info">
+            <h4 style={{marginBottom:"0"}}>Example 3</h4>
             <p style={{margin:"0"}}>If you like original gut wrenching laughter you will like this movie. If you are young or old then you will love this movie, hell even my mom liked it.Great Camp!!!</p>
+            </ListGroup.Item>
+            <ListGroup.Item variant="info">
             <h4 style={{marginBottom:"0"}}>Example 4</h4>
             <p style={{margin:"0"}}>I saw this movie when I was about 12 when it came out. I recall the scariest scene was the big bird eating men dangling helplessly from parachutes 
               right out of the air. The horror. The horror.As a young kid going to these cheesy B films on Saturday afternoons, I still was tired of the 
@@ -27,6 +40,8 @@ class EXAMPLE extends Component {
               unintentional humor.But, about a year or so later, I saw Psycho when it came out and I loved that the star, Janet Leigh, was bumped off 
               early in the film. I sat up and took notice at that point. Since screenwriters are making up the story, make it up to be as scary as possible and not 
               from a well-worn formula. There are no rules.</p>
+              </ListGroup.Item>
+              </ListGroup>
           </div>
     );
   }
--- Webapp/React/src/components/main.js
+++ Webapp/React/src/components/main.js
@@ -2,16 +2,47 @@ import React, { Component } from "react";
 import PREDICTION from "./prediction";
 import EXAMPLE from "./example";
 
+import {
+  Navbar,
+  NavDropdown,
+  Nav
+  } from "react-bootstrap";
+
 class Main extends Component {
+
   render() {
     return (
-        <div style={{display:"flex",justifyContent:"center", flexDirection:"row", padding:"10px", height:"auto", width:"auto"}}>
+      <div>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Senti-review</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#">Jupyter Notebook</Nav.Link>
+                <NavDropdown title="Tutorial Chapters" id="collasible-nav-dropdown">
+                  <NavDropdown.Item href="#">Ch-1</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Ch-2</NavDropdown.Item>
+                  <NavDropdown.Item href="#">Ch-3</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Nav>
+              <Nav.Link href="https://www.imdb.com/">Best Movies review</Nav.Link>
+                <Nav.Link eventKey={2} href="https://github.com/ahmadkhan242/Transfer-Learning-Model-hosted-on-Heroku-using-React-Flask">
+                  Github
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+      
+        <div style={{display:"flex",justifyContent:"center", flexDirection:"row", padding:"10px", height:"auto", width:"auto", background:"#B3E5FC"}}>
+          
             <div style={{width:"50%"}}>
                 <PREDICTION></PREDICTION>
             </div>
             <div style={{width:"50%"}}>
                 <EXAMPLE></EXAMPLE>
             </div>
+        </div>
         </div>
     );
   }
